24f1f423f67d9aa9560135009773ed12db8b0017
Pavel Dadykin <pavel.splean@gmail.com>
2013-07-18 11:06:54 +0200

    Add first passing test for env

--- lib/configus/builder.rb
+++ lib/configus/builder.rb
@@ -1,4 +1,19 @@
 module Configus
   class Builder
+
+    def initialize
+      @hash = {}
+    end
+
+    def build(env)
+      @hash[env.to_sym] = {}
+      @hash 
+    end
+
+    def env(name, options = {})
+      @hash[name.to_sym] = options
+      @hash
+    end
+
   end
 end
--- test/configus/builder_test.rb
+++ test/configus/builder_test.rb
@@ -8,4 +8,10 @@ describe Configus::Builder do
   it "should create hash" do
     @config.build(:develop).must_be_kind_of Hash 
   end
+
+  it "should add options to environment" do
+    expected_hash = {develop: {email: "admin@gmail.com"}}
+    @config.env(:develop, email: "admin@gmail.com").must_equal expected_hash
+  end
+
 end
